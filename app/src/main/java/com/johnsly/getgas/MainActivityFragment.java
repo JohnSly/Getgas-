fd7c4f6d1252c3f88156d751684e654eb42436cc
Sylvia <sylviakieha@gmail.com>
2015-07-30 19:13:46 +0300

    added profile tracker

--- app/src/main/java/com/johnsly/getgas/MainActivityFragment.java
+++ app/src/main/java/com/johnsly/getgas/MainActivityFragment.java
@@ -10,11 +10,13 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.Profile;
+import com.facebook.ProfileTracker;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
@@ -25,8 +27,11 @@ import com.facebook.login.widget.LoginButton;
 public class MainActivityFragment extends Fragment {
 
     private TextView mTextDetails;
-
     private CallbackManager mcallbackManager;
+
+    private AccessTokenTracker mTokenTracker;
+    private ProfileTracker mProfileTracker;
+
     private FacebookCallback<LoginResult> mCallback = new FacebookCallback<LoginResult>() {
         @Override
         public void onSuccess(LoginResult loginResult) {
@@ -56,6 +61,22 @@ public class MainActivityFragment extends Fragment {
         // Initialize the SDK before executing any other operations,
         // especially, if you're using Facebook UI elements.
         mcallbackManager = CallbackManager.Factory.create();
+        AccessTokenTracker tracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken old, AccessToken newToken) {
+
+            }
+        };
+
+        ProfileTracker profileTracker = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(Profile oldProfile, Profile newProfile) {
+              displayWelcomeMessage(newProfile);
+            }
+        };
+
+        mTokenTracker.startTracking();
+        mProfileTracker.startTracking();
     }
 
     @Override
@@ -84,6 +105,13 @@ public class MainActivityFragment extends Fragment {
         super.onResume();
         Profile profile = Profile.getCurrentProfile();
         displayWelcomeMessage(profile);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mTokenTracker.stopTracking();
+        mProfileTracker.startTracking();
     }
 
     @Override
