5042afffdec8c903e4cc0a6920e9d6dfa6824aca
Sylvia <sylviakieha@gmail.com>
2015-07-30 17:42:37 +0300

    initialized callbackmanager

--- app/src/main/java/com/johnsly/getgas/MainActivityFragment.java
+++ app/src/main/java/com/johnsly/getgas/MainActivityFragment.java
@@ -1,12 +1,21 @@
 package com.johnsly.getgas;
 
+import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
 
@@ -14,6 +23,30 @@ import com.facebook.login.widget.LoginButton;
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private TextView mTextDetails;
+
+    private CallbackManager mcallbackManager;
+    private FacebookCallback<LoginResult> mCallback = new FacebookCallback<LoginResult>() {
+        @Override
+        public void onSuccess(LoginResult loginResult) {
+            AccessToken accessToken = loginResult.getAccessToken();
+            Profile profile = Profile.getCurrentProfile();
+            if (profile != null) {
+                mTextDetails.setText("Welcome " + profile.getName());
+            }
+        }
+
+        @Override
+        public void onCancel() {
+
+        }
+
+        @Override
+        public void onError(FacebookException e) {
+
+        }
+    };
 
     public MainActivityFragment() {
     }
@@ -24,6 +57,7 @@ public class MainActivityFragment extends Fragment {
         FacebookSdk.sdkInitialize(getActivity().getApplicationContext());
         // Initialize the SDK before executing any other operations,
         // especially, if you're using Facebook UI elements.
+        mcallbackManager = CallbackManager.Factory.create();
     }
 
     @Override
@@ -36,5 +70,14 @@ public class MainActivityFragment extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         LoginButton loginButton = (LoginButton) view.findViewById(R.id.login_button);
+        loginButton.setReadPermissions("user_friends");
+        loginButton.setFragment(this);
+        loginButton.registerCallback(mcallbackManager, mCallback);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mcallbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
