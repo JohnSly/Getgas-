87338f1d0bd53dc9cc00182e3b8619d05930686d
wolfieorama <wachirawamunyi@gmail.com>
2015-07-31 15:07:31 +0300

    added floating action button

--- app/src/main/java/com/johnsly/getgas/Input.java
+++ app/src/main/java/com/johnsly/getgas/Input.java
@@ -5,6 +5,11 @@ import android.support.v7.app.ActionBarActivity;
 import android.widget.ImageView;
 
 import com.oguzdev.circularfloatingactionmenu.library.FloatingActionButton;
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionMenu;
+import com.oguzdev.circularfloatingactionmenu.library.SubActionButton;
+
+import static com.johnsly.getgas.R.drawable.next;
+import static com.johnsly.getgas.R.drawable.prev;
 
 /**
  * Created by njerry on 7/31/15.
@@ -22,6 +27,24 @@ public class Input extends ActionBarActivity {
         FloatingActionButton actionButton = new FloatingActionButton.Builder(this)
                 .setContentView(icon)
                 .build();
+
+        SubActionButton.Builder itemBuilder = new SubActionButton.Builder(this);
+        // repeat many times:
+        ImageView itemIcon = new ImageView(this);
+        itemIcon.setImageResource(next);
+        SubActionButton button1 = itemBuilder.setContentView(itemIcon).build();
+
+        ImageView itemIcon2 = new ImageView(this);
+        itemIcon.setImageResource(prev);
+        SubActionButton button2 = itemBuilder.setContentView(itemIcon2).build();
+
+        FloatingActionMenu actionMenu = new FloatingActionMenu.Builder(this)
+                .addSubActionView(button2)
+                .addSubActionView(button1)
+
+                .attachTo(actionButton)
+                .build();
+
 
     }
 
